3f542c2c7551bcecad813c599b86126561a61a3d
Marko_Askovic <marko.askovic@casasoft.com>
2023-07-27 12:47:33 +0200

    tests finished

--- projects/crm/src/app/auth/auth.service.spec.ts
+++ projects/crm/src/app/auth/auth.service.spec.ts
@@ -1,7 +1,11 @@
-import { SpectatorHttp, createHttpFactory } from '@ngneat/spectator';
+import {
+  HttpMethod,
+  SpectatorHttp,
+  createHttpFactory,
+} from '@ngneat/spectator';
 import { AuthService, TOKEN_MANAGER } from './auth.service';
 import { TokenManager } from './token-manager';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, combineLatest, of, skip } from 'rxjs';
 
 let storedToken: string | null = null;
 
@@ -64,5 +68,73 @@ describe('AuthService', () => {
       expect(status).toBeFalse();
       done();
     });
+  });
+
+  it('should store token and set authStatus$ to TRUE if login() succeeds', (done: DoneFn) => {
+    spectator = createSpectator();
+
+    const login$ = spectator.service.login({
+      email: 'mock@mail.com',
+      password: 'passw0rd',
+    });
+
+    const authStatus$ = spectator.service.authStatus$.pipe(skip(1));
+
+    combineLatest([login$, authStatus$]).subscribe(([token, status]) => {
+      expect(token).toBe('MOCK_TOKEN');
+      expect(storedToken).toBe('MOCK_TOKEN');
+      expect(status).toBeTrue();
+      done();
+    });
+
+    const req = spectator.expectOne(
+      'https://x8ki-letl-twmt.n7.xano.io/api:cLAOENeS/auth/login',
+      HttpMethod.POST
+    );
+
+    req.flush({
+      authToken: 'MOCK_TOKEN',
+    });
+  });
+
+  it('should not store token and authStatus$ should remain FALSE if login() fails', (done: DoneFn) => {
+    spectator = createSpectator();
+
+    const login$ = spectator.service
+      .login({
+        email: 'mock@mail.com',
+        password: 'passw0rd',
+      })
+      .pipe(catchError(() => of('error')));
+
+    const authStatus$ = spectator.service.authStatus$;
+
+    combineLatest([login$, authStatus$]).subscribe(
+      ([token, status]) => {
+        // expect(token).toBe('error');
+        expect(status).toBeFalse();
+        expect(storedToken).toBeNull();
+        done();
+      }
+      /*       error: () => {
+        expect(storedToken).toBeNull();
+        done();
+      }, */
+    );
+
+    const req = spectator.expectOne(
+      'https://x8ki-letl-twmt.n7.xano.io/api:cLAOENeS/auth/login',
+      HttpMethod.POST
+    );
+
+    req.flush(
+      {
+        message: 'MOCK_ERROR_MESSAGE',
+      },
+      {
+        status: 401,
+        statusText: 'Unauthorized',
+      }
+    );
   });
 });
