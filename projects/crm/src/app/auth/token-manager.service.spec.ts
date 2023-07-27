1dec4332fb265adb1f18c9bc5ee4355fedb784b4
Marko_Askovic <marko.askovic@casasoft.com>
2023-07-27 10:53:35 +0200

    tests continued

--- projects/crm/src/app/auth/token-manager.service.spec.ts
+++ projects/crm/src/app/auth/token-manager.service.spec.ts
@@ -1,11 +1,90 @@
-import { LocalStorageTokenManager } from './token-manager.service';
+import {
+  LocalStorageTokenManager,
+  SessionStorageTokenManager,
+} from './token-manager.service';
 
 describe('LocalStorageTokenManager', () => {
-  it('should store token in localStorage', () => {
-    const service = new LocalStorageTokenManager();
+  let service: LocalStorageTokenManager;
 
-    service.storeToken('MOCK_TOKEN');
+  beforeEach(() => {
+    service = new LocalStorageTokenManager();
+    window.localStorage.removeItem('authToken');
+  });
+
+  it('should store token in localStorage and return an observable of the token', (done: DoneFn) => {
+    service.storeToken('MOCK_TOKEN').subscribe((token) => {
+      expect(token).toBe('MOCK_TOKEN');
+      done();
+    });
 
     expect(window.localStorage.getItem('authToken')).toBe('MOCK_TOKEN');
+  });
+
+  it('should load a token from the localStorage (if there is one)', (done: DoneFn) => {
+    window.localStorage.setItem('authToken', 'MOCK_TOKEN');
+
+    service.loadToken().subscribe((token) => {
+      expect(token).toBe('MOCK_TOKEN');
+      done();
+    });
+  });
+
+  it('should load null if no token is stored in the localStorage', (done: DoneFn) => {
+    service.loadToken().subscribe((token) => {
+      expect(token).toBe(null);
+      done();
+    });
+  });
+
+  it('should remove token from localStorage', (done: DoneFn) => {
+    window.localStorage.setItem('authToken', 'MOCK_TOKEN');
+
+    service.removeToken().subscribe(() => {
+      expect(window.localStorage.getItem('authToken')).toBeNull();
+      done();
+    });
+  });
+});
+
+describe('SessionStorageTokenManager', () => {
+  let service: SessionStorageTokenManager;
+
+  beforeEach(() => {
+    service = new SessionStorageTokenManager();
+    window.sessionStorage.removeItem('authToken');
+  });
+
+  it('should store token in sessionStorage and return an observable of the token', (done: DoneFn) => {
+    service.storeToken('MOCK_TOKEN').subscribe((token) => {
+      expect(token).toBe('MOCK_TOKEN');
+      done();
+    });
+
+    expect(window.sessionStorage.getItem('authToken')).toBe('MOCK_TOKEN');
+  });
+
+  it('should load a token from the sessionStorage (if there is one)', (done: DoneFn) => {
+    window.sessionStorage.setItem('authToken', 'MOCK_TOKEN');
+
+    service.loadToken().subscribe((token) => {
+      expect(token).toBe('MOCK_TOKEN');
+      done();
+    });
+  });
+
+  it('should load null if no token is stored in the sessionStorage', (done: DoneFn) => {
+    service.loadToken().subscribe((token) => {
+      expect(token).toBe(null);
+      done();
+    });
+  });
+
+  it('should remove token from sessionStorage', (done: DoneFn) => {
+    window.sessionStorage.setItem('authToken', 'MOCK_TOKEN');
+
+    service.removeToken().subscribe(() => {
+      expect(window.sessionStorage.getItem('authToken')).toBeNull();
+      done();
+    });
   });
 });
