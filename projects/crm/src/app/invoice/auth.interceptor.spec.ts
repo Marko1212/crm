e881f3681f7b2273506907045b1fd49b09652414
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-04 09:15:30 +0200

    AuthInterceptor tests finished

--- projects/crm/src/app/invoice/auth.interceptor.spec.ts
+++ projects/crm/src/app/invoice/auth.interceptor.spec.ts
@@ -7,6 +7,7 @@ import { AuthInterceptor } from './auth.interceptor';
 import { AuthService } from '../auth/auth.service';
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import { of } from 'rxjs';
 
 describe('AuthInterceptor', () => {
   let spectator: SpectatorHttp<AuthInterceptor>;
@@ -36,5 +37,39 @@ describe('AuthInterceptor', () => {
     );
 
     expect(req.request.headers.has('Authorization')).toBeFalse();
+  });
+
+  it('should throw an error if we reach /invoice without a authToken', (done: DoneFn) => {
+    spectator = createSpectator();
+
+    const http = spectator.inject(HttpClient);
+    const auth = spectator.inject(AuthService);
+
+    spyOnProperty(auth, 'authToken').and.returnValue(of(null));
+
+    http.get(environment.apiUrl + '/invoice/toto').subscribe({
+      error: (error) => {
+        expect(error.message).toBe("Aucun token d'authentification");
+        done();
+      },
+    });
+  });
+
+  it('should add Authorization Bearer if we reach /invoice with an authToken', () => {
+    spectator = createSpectator();
+
+    const http = spectator.inject(HttpClient);
+    const auth = spectator.inject(AuthService);
+
+    spyOnProperty(auth, 'authToken').and.returnValue(of('MOCK_TOKEN'));
+
+    http.get(environment.apiUrl + '/invoice/toto').subscribe();
+
+    const req = spectator.expectOne(
+      environment.apiUrl + '/invoice/toto',
+      HttpMethod.GET
+    );
+    expect(req.request.headers.has('Authorization')).toBeTrue();
+    expect(req.request.headers.get('Authorization')).toBe('Bearer MOCK_TOKEN');
   });
 });
