87c53dcb61255bfce905cde45a3898c51f89d072
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-06 16:10:44 +0200

    update d'un test

--- projects/crm/src/app/invoice/invoice.service.spec.ts
+++ projects/crm/src/app/invoice/invoice.service.spec.ts
@@ -2,30 +2,30 @@ import {
   HttpMethod,
   SpectatorHttp,
   createHttpFactory,
-} from "@ngneat/spectator";
-import { InvoiceService } from "./invoice.service";
-import { Invoice } from "./invoice";
-import { environment } from "../../environments/environment.development";
+} from '@ngneat/spectator';
+import { InvoiceService } from './invoice.service';
+import { Invoice } from './invoice';
+import { environment } from '../../environments/environment.development';
 
-describe("InvoiceService", () => {
+describe('InvoiceService', () => {
   let spectator: SpectatorHttp<InvoiceService>;
 
   const createSpectator = createHttpFactory({
     service: InvoiceService,
   });
 
-  it("should call /invoice in POST when we call create()", () => {
+  it('should call /invoice in POST when we call create()', () => {
     spectator = createSpectator();
 
     const invoice: Invoice = {
-      description: "MOCK_DESCRIPTION",
-      customer_name: "MOCK_CUSTOMER",
-      status: "PAID",
+      description: 'MOCK_DESCRIPTION',
+      customer_name: 'MOCK_CUSTOMER',
+      status: 'PAID',
       details: [
         {
           quantity: 3,
           amount: 300,
-          description: "MOCK_DETAIL_DESCRIPTION",
+          description: 'MOCK_DETAIL_DESCRIPTION',
         },
       ],
     };
@@ -33,10 +33,21 @@ describe("InvoiceService", () => {
     spectator.service.create(invoice).subscribe();
 
     const req = spectator.expectOne(
-      environment.apiUrl + "/invoice",
+      environment.apiUrl + '/invoice',
       HttpMethod.POST
     );
 
-    expect(req.request.body).toEqual(invoice);
+    expect(req.request.body).toEqual({
+      description: 'MOCK_DESCRIPTION',
+      customer_name: 'MOCK_CUSTOMER',
+      status: 'PAID',
+      details: [
+        {
+          quantity: 3,
+          amount: 30000,
+          description: 'MOCK_DETAIL_DESCRIPTION',
+        },
+      ],
+    });
   });
 });
