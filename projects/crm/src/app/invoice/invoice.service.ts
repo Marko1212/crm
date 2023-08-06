9a35b28734c9d05862a3d101ed9d7752bc19ec01
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-06 15:54:45 +0200

    juste quelques tests et c'est fini

--- projects/crm/src/app/invoice/invoice.service.ts
+++ projects/crm/src/app/invoice/invoice.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Invoice } from './invoice';
 import { AuthService } from '../auth/auth.service';
-import { switchMap, tap } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 
 const API_URL = environment.apiUrl;
@@ -12,25 +12,56 @@ export class InvoiceService {
   constructor(private http: HttpClient) {}
 
   create(invoiceData: Invoice) {
-    return this.http.post<Invoice>(API_URL + '/invoice', invoiceData);
+    const finalInvoice: Invoice = this.mapAppInvoiceToApiInvoice(invoiceData);
+    return this.http.post<Invoice>(API_URL + '/invoice', finalInvoice);
   }
 
   update(invoiceData: Invoice) {
+    const finalInvoice: Invoice = this.mapAppInvoiceToApiInvoice(invoiceData);
     return this.http.put<Invoice>(
       API_URL + '/invoice/' + invoiceData.id,
-      invoiceData
+      finalInvoice
     );
   }
 
   findAll() {
-    return this.http.get<Invoice[]>(API_URL + '/invoice');
+    return this.http.get<Invoice[]>(API_URL + '/invoice').pipe(
+      map((invoices) => {
+        return invoices.map((invoice) => {
+          return { ...invoice, total: invoice.total! / 100 };
+        });
+      })
+    );
   }
 
   find(id: number) {
-    return this.http.get<Invoice>(API_URL + '/invoice/' + id);
+    return this.http
+      .get<Invoice>(API_URL + '/invoice/' + id)
+      .pipe(map((invoice) => this.mapApiInvoiceToAppInvoice(invoice)));
   }
 
   delete(id: number) {
     return this.http.delete(API_URL + '/invoice/' + id);
+  }
+
+  mapApiInvoiceToAppInvoice(invoice: Invoice) {
+    return {
+      ...invoice,
+      details: invoice.details.map((item) => {
+        return { ...item, amount: item.amount / 100 };
+      }),
+    };
+  }
+
+  mapAppInvoiceToApiInvoice(invoice: Invoice) {
+    return {
+      ...invoice,
+      details: invoice.details.map((item) => {
+        return {
+          ...item,
+          amount: item.amount * 100,
+        };
+      }),
+    };
   }
 }
